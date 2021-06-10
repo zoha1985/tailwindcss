c6efd533667d8fb95649a0d8e6286f1b439b7450
zoha1985 <shumsuzzuha@gmail.com>
2021-06-10 13:30:53 +0600

    anpther

--- src/Component/Clint.js
+++ src/Component/Clint.js
@@ -1,17 +1,20 @@
 import React from 'react';
 
 const Clint = ( props ) => {
-const{ title, body} = props.newCard;
-console.log(title)
+const{ name, username, email, address} = props.newCard;
+// console.log(title)
     return (
     
         <div>
         <div class="border-red-500 border-4 border-opacity-10 p-4 rounded-lg" >
        
             <img class="rounded-md bg-blue-700 p-2" src="https://paramountdbs.com/images/led-tv-39.jpg" alt="" width="300" height="400" />
-                <h1 class="text-2xl">Title here : {title} </h1>
+                <h1 class="text-2xl mt-4">Title here : {name} </h1>
+                <h1 class="text-1xl">Title here : {username} </h1>
+                <h1 class="text-1xl">Title here : {email} </h1>
+                {/* <h1 class="text-2xl">Title here : {address} </h1> */}
                 <br/>
-                <h6>{body}</h6>
+                {/* <h6>{body}</h6> */}
       
         </div>
     </div>
--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     const [newCard, setNewCard] = useState([]);
 
     useEffect( () => {
-        fetch('https://jsonplaceholder.typicode.com/posts')
+        fetch('https://jsonplaceholder.typicode.com/users')
         .then(res => res.json())
         .then(data =>{
           console.log("coppy", data)
